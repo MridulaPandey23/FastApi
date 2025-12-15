72943fefa395b164e1078dd220bb79eac9b6adb9
Mridula Pandey <pandeymridula23@gmail.com>
2025-12-15 17:28:21 +0530

    CRUD

--- main.py
+++ main.py
@@ -2,6 +2,26 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+Static_db = {"name1": "ABC", "age1": 30, "gender1": "male"}
+
+
+@app.post("/name,age,gender")
+def add_details(name: str, age: int, gender: str):
+    Static_db['name2'] = name
+    Static_db['age2'] = age
+    Static_db['gender2'] = gender
+    return Static_db
+
+@app.put("/name,age,gender")
+def update_details(name: str):
+    Static_db['name1'] = name
+    return Static_db
+
+@app.delete("/gender")
+def delete_details():
+    del Static_db['gender1']
+    return Static_db
+
 @app.get("/")
-def root():
-    return {" Hello" :" World!"}
+def get_details():
+    return Static_db
