574aa6e60aca241d3b12db2ca8f1c553c4c2e386
Mridula Pandey <pandeymridula23@gmail.com>
2025-12-16 12:22:12 +0530

    ErrorHandling

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-Static_db = [{'id': 1, 'name': 'Alice', 'age': 30, 'gender': 'Female'},
-             {'id': 2, 'name': 'Bob', 'age': 25, 'gender': 'Male'}
+user_details = [{'id': 0, 'name': 'ABC', 'age': 30, 'gender': 'Female'},
+             {'id': 1, 'name': 'PQR', 'age': 25, 'gender': 'Male'}
 ]
 
 @app.post("/")
 def add_details(name: str, age: int, gender: str):
-   newId = len(Static_db) + 1
-   Static_db.append({'id': newId, 'name': name, 'age': age, 'gender': gender})
-   return Static_db
+   newId = len(user_details)
+   if any(user['name'] == name for user in user_details):
+       raise HTTPException(status_code=400, detail="User with this name already exists")
+   else:
+        user_details.append({'id': newId, 'name': name, 'age': age, 'gender': gender})
+   return user_details
 
 @app.put("/")
 def update_details(id: int, name: str, age: int, gender: str):
-    Static_db[id] = {'id': id, 'name': name, 'age': age, 'gender': gender}
-    return Static_db
+    if any(user['id']==id for user in user_details):
+        user_details[id] = {'id': id, 'name': name, 'age': age, 'gender': gender}
+    else:
+        raise HTTPException(status_code=404, detail="No such user present")
+    return user_details
 
-@app.delete("/")
+@app.delete("/{id}")
 def delete_details(id: int):
-    del Static_db[id]
-    return Static_db
+    if any(user['id']==id for user in user_details):
+        del user_details[id]
+    else:
+        raise HTTPException(status_code=404, detail="No such user present")
+    return user_details
 
 @app.get("/")
 def get_details():
-    return Static_db
+    return user_details
